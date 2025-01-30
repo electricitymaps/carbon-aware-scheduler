416b386676fc79c022c09f6023ea5d2eb2216114
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2025-01-30 16:26:24 +0100

    simplify to deploy it

--- src/lib/scheduler.py
+++ src/lib/scheduler.py
@@ -3,4 +3,4 @@ from src.lib.task import CarbonAwareTask
 
 def determine_optimal_execution_time(task: CarbonAwareTask) -> datetime:
     """Determines the optimal time to execute a CarbonAwareTask using the carbon aware sdk."""
-    raise NotImplementedError()
+    pass
