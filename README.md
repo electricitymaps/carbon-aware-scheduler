7748024c671b4c7366893e7d5103922a65f62f7e
Robin TROESCH <38283096+unitrium@users.noreply.github.com>
2025-01-30 12:06:59 +0100

    add terraform infra

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # carbon-aware-scheduler
 A cloud carbon aware scheduler using the carbon aware sdk
+
+## Initial infrastructure setup
+
+First export the following environment variables:
+```
+export TF_VAR_region=<your gcp region>
+export TF_VAR_project_id=<your gcp project id>
+```
+Then run the following commands:
+```bash
+terraform init
+terraform plan
+terraform apply
+```
---
+++ gcp_infrastructure.tf
@@ -0,0 +1,32 @@
+variable "region" {
+  type = string
+}
+
+variable "project_id" {
+  type = string
+}
+
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+}
+
+
+resource "google_storage_bucket" "tasks_bucket" {
+  name          = "carbon-aware-tasks"
+  location      = var.region
+  storage_class = "STANDARD"
+
+  public_access_prevention = "enforced"
+}
+
+resource "google_pubsub_topic" "carbon_aware_tasks_to_schedule" {
+  name = "carbon-aware-tasks-to-schedule"
+
+  labels = {
+    usage = "carbon-aware-scheduler"
+  }
+
+  message_retention_duration = "7200s"
+}
